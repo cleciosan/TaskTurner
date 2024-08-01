560cc062f75fbaa10a9d999a371e77d50199b36e
Clecio Batista Santos <cleciosan@hotmail.com>
2024-08-01 09:20:33 -0300

    Add Enum TaskCategory to Task class

--- TaskTurner/Models/Task.cs
+++ TaskTurner/Models/Task.cs
@@ -34,5 +34,81 @@ namespace TaskTurner.Models
         Deleted
     }
 
+    public enum TaskCategory
+    {
+        /// <summary>
+        /// Task rekated to your job, like meetings, project deadlines, or professional development activities.
+        /// </summary>
+        Work,
 
+        /// <summary>
+        /// Daily routines, hobbies, or personal goals
+        /// </summary>
+        Personal,
+
+        /// <summary>
+        /// Household chores, repairs, or home improvement projects.
+        /// </summary>
+        Home,
+        
+        /// <summary>
+        /// Exercise routines, meal planning, doctor's appointments, or meditation.
+        /// </summary>
+        HealthWelbeing,
+        
+        /// <summary>
+        /// Budgeting, bill payments, and financial planning.
+        /// </summary>
+        Finance,
+        
+        /// <summary>
+        /// Grocery lists, clothing, or other shopping needs.
+        /// </summary>
+        Shopping,
+        
+        /// <summary>
+        /// Family gatherings, social events, or activities with friends.
+        /// </summary>
+        SocialFamily,
+        
+        /// <summary>
+        /// Study sessions, assignments, or educational goals.
+        /// </summary>
+        Education,
+        
+        /// <summary>
+        /// Planning for trips, packing lists, or travel itineraries.
+        /// </summary>
+        Travel,
+
+        /// <summary>
+        /// Tasks like going to the post office, bank, or dry cleaners.
+        /// </summary>
+        Errands,
+
+        /// <summary>
+        /// Time set aside for hobbies or leisure activities.
+        /// </summary>
+        HobbiesLeisure,
+
+        /// <summary>
+        /// Community services or volunteering activities.
+        /// </summary>
+        VolunteeringCommunity,
+
+        /// <summary>
+        /// Special dates and celebrations.
+        /// </summary>
+        BirthdaysAnniversaries,
+
+        /// <summary>
+        /// Larger tasks that might span over multiple days or weeks.
+        /// </summary>
+        Projects,
+
+        /// <summary>
+        /// Objectives or goals that you're working towards over a longer period.
+        /// </summary>
+        LongTermGoals
+    }
 }
