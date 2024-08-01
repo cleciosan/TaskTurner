e7854150f9206c1a6130e63e0084bd66ab58a117
Clecio Batista Santos <cleciosan@hotmail.com>
2024-08-01 09:41:50 -0300

    Add TaskChecklist class in Models folder

---
+++ TaskTurner/Models/TaskChecklist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTurner.Models
+{
+    public class TaskChecklist
+    {
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+
+    }
+}
